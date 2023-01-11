338d2b7e37efdce9e0fa2d09fab37c765bf6dcac
vincowl <vincent@heliosphere.fr>
2023-01-11 16:40:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     ],
     extras_require={
         "ui": [
-            "dbus-python >= 1.2, < 1.3",
+            "dbus-python >= 1.2, <= 1.3",
             "PyGObject >= 3.36, < 3.39",
             "pydbus >= 0.6, < 0.7",
             "notify2 >= 0.3, < 0.4",
