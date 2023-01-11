5edf8fc5fe67168b8ed10c94883ff0a39a1f8792
vincowl <vincent@heliosphere.fr>
2023-01-11 18:27:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
             "dbus-python >= 1.2",
             "PyGObject >= 3.36",
             "pydbus >= 0.6",
-            "notify2 >= 0.3",
+            "notify2 >= 0.2"
         ]
     },
     entry_points={
